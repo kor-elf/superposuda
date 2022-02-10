9769c85b10b778519d36f9133f7b542e19cfe70e
Leonid Nikitin <i@kor-elf.net>
2022-02-11 02:03:34 +0600

    Added param timeout to config retailcrm.

--- config/retailcrm.php
+++ config/retailcrm.php
@@ -2,5 +2,7 @@
 
 return [
     'token' => env('RETAILCRM_TOKEN', ''),
-    'url' => 'https://superposuda.retailcrm.ru/',
+    'url' => 'https://superposuda.retailcrm.ru',
+    // Максимальное время, которое ждём
+    'timeout' => env('RETAILCRM_MAX_TIMEOUT', 60),
 ];
