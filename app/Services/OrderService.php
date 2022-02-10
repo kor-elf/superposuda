05b4843a127147726fce9fb85494d8fdd78ab0dc
Leonid Nikitin <i@kor-elf.net>
2022-02-11 02:06:55 +0600

    Added service for order.

---
+++ app/Services/OrderService.php
@@ -0,0 +1,41 @@
+<?php
+namespace App\Services;
+
+use App\Dto\OrderCreateDto;
+use App\Helpers\FunctionResult;
+use App\Services\Retailcrm\RetailcrmSearchService;
+use App\Services\Retailcrm\RetailcrmService;
+
+class OrderService
+{
+    private RetailcrmSearchService $retailcrmSearchService;
+    private RetailcrmService $retailcrmService;
+
+    public function __construct(RetailcrmSearchService $retailcrmSearchService, RetailcrmService $retailcrmService)
+    {
+        $this->retailcrmSearchService = $retailcrmSearchService;
+        $this->retailcrmService = $retailcrmService;
+    }
+
+    public function create(OrderCreateDto $orderCreateDto): FunctionResult
+    {
+        $article = $orderCreateDto->getArticle();
+        $manufacturer = $orderCreateDto->getManufacturer();
+        $product = $this->retailcrmSearchService->searchProduct($article, $manufacturer);
+
+        if (!$product->isSuccess()) {
+            return $product;
+        }
+        $product = $product->getReturnValue();
+        $products = [
+            $product['id']
+        ];
+
+        $order = $this->retailcrmService->order($orderCreateDto, $products);
+        if (!$order->isSuccess()) {
+            return $order;
+        }
+
+        return FunctionResult::success('Заказ успешно создан');
+    }
+}
