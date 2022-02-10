175d140595d40e85ce618c1b1b4f6802960cdc6a
Leonid Nikitin <i@kor-elf.net>
2022-02-11 02:05:29 +0600

    Added DTO for order.

---
+++ app/Dto/OrderCreateDto.php
@@ -0,0 +1,59 @@
+<?php
+namespace App\Dto;
+
+class OrderCreateDto
+{
+    private string $lastName;
+    private string $firstName;
+    private string $patronymic;
+    private string $comment;
+    private string $article;
+    private string $manufacturer;
+
+    public function __construct(
+        string $lastName,
+        string $firstName,
+        string $patronymic,
+        string $comment,
+        string $article,
+        string $manufacturer
+    )
+    {
+        $this->lastName = $lastName;
+        $this->firstName = $firstName;
+        $this->patronymic = $patronymic;
+        $this->comment = $comment;
+        $this->article = $article;
+        $this->manufacturer = $manufacturer;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function getPatronymic(): string
+    {
+        return $this->patronymic;
+    }
+
+    public function getComment(): string
+    {
+        return $this->comment;
+    }
+
+    public function getArticle(): string
+    {
+        return $this->article;
+    }
+
+    public function getManufacturer(): string
+    {
+        return $this->manufacturer;
+    }
+}
