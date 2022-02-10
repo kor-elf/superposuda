7460b84f18de3d71badbb036c2f272c571600a48
Leonid Nikitin <i@kor-elf.net>
2022-02-11 02:02:51 +0600

    Added alerts to layout app.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{ config('app.name', 'Laravel') }}</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     </head>
     <body>
         <main>
-            @yield('content')
+            <div class="container">
+                @include('layouts._alerts')
+                @yield('content')
+            </div>
         </main>
     </body>
 </html>
